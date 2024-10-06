86f59125c06e5b2e979b2fdd3a46e5d80bf4079e
ArcherStudio1111 <146674268+T-Z-RYU@users.noreply.github.com>
2024-10-07 00:07:21 +0800

    Change to 3rd perspective

--- Assets/Scripts/Church/ChurchPlayer.cs
+++ Assets/Scripts/Church/ChurchPlayer.cs
@@ -1,5 +1,6 @@
 using System;
 using DG.Tweening;
+using Unity.VisualScripting;
 using UnityEngine;
 
 namespace Church
@@ -11,6 +12,7 @@ namespace Church
         [SerializeField] private Transform mainCamera;
         [SerializeField] private Transform mollyPos;
         [SerializeField] private Rigidbody playerRb;
+        [SerializeField] private Animator playerAnim;
 
         private bool _isFalling;
         
@@ -32,6 +34,29 @@ namespace Church
             if (_isFalling)
             {
                 mainCamera.LookAt(mollyPos);
+            }
+
+            MoveAnim();
+        }
+
+        private void MoveAnim()
+        {
+            if (Input.GetAxisRaw("Horizontal").Equals(0) && Input.GetAxisRaw("Vertical").Equals(0))
+            {
+                playerAnim.SetBool("IsWalking", false);
+            }
+            else
+            {
+                playerAnim.SetBool("IsWalking", true);
+            }
+
+            if (Input.GetAxisRaw("Vertical").Equals(1))
+            {
+                playerAnim.transform.rotation = Quaternion.Euler(new Vector3(0, 180, 0));
+            }
+            if (Input.GetAxisRaw("Vertical").Equals(-1))
+            {
+                playerAnim.transform.rotation = Quaternion.Euler(Vector3.zero);
             }
         }
     }
