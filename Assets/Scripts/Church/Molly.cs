4c305e3155c4508faaf36423d60236f4bbcf3fc5
ArcherStudio1111 <146674268+T-Z-RYU@users.noreply.github.com>
2024-10-08 00:40:54 +0800

    Adjust Bruce and Ocean scenes

--- Assets/Scripts/Church/Molly.cs
+++ Assets/Scripts/Church/Molly.cs
@@ -6,6 +6,8 @@ namespace Church
 {
     public class Molly : MonoBehaviour
     {
+        public Transform[] pathPos;
+        
         private bool _isNearPlayer;
         private bool _isRising;
         
@@ -28,8 +30,15 @@ namespace Church
         private void OnRiseEvent()
         {
             _isRising = true;
-            transform.parent = GameObject.FindGameObjectWithTag("Player").transform;
-            transform.DOLocalMove(new Vector3(0.27363804f,1.48200035f,3.35067821f), 1f);
+            var pathVectors = new Vector3[10];
+            for (var i = 0; i < pathVectors.Length; i++)
+            {
+                pathVectors[i] = pathPos[i].position + new Vector3(0, 3f, 0);
+            }
+
+            transform.DOPath(pathVectors, 22.5f);
+            // transform.parent = GameObject.FindGameObjectWithTag("Player").transform;
+            // transform.DOLocalMove(new Vector3(0.27363804f,1.48200035f,3.35067821f), 1f);
         }
 
         private void OnFallEvent()
